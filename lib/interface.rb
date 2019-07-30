ab0398a8fdd4ce3e2ab24e106e8926388d30a1ba
Gulnoza Muminova <gmcharmy@gmail.com>
2019-07-30 10:44:28 -0400

    Edit Interface

--- lib/interface.rb
+++ lib/interface.rb
@@ -5,8 +5,8 @@ class Interface
       @prompt = TTY::Prompt.new
     end
     def welcome
-        puts "Welcome to the Hero Application! 💪"
-          answer = prompt.select("Are you a returning user or are you a new user?") do |menu|
+        puts "Welcome to the Toro Application!"
+          answer = prompt.select("Have you been here before?") do |menu|
           menu.choice "New User", -> {User.handle_new_user}
           menu.choice "Returning User", -> {User.handle_returning_user}
         end
@@ -15,11 +15,12 @@ class Interface
     def main_menu
         system "clear"
         user.reload
-        prompt.select("Welcome #{self.user.name}! What would you like to do today?") do |menu|
+        prompt.select("Welcome #{self.user.name}! are you ready to learn your fortune? Choose 4 cards, please.") do |menu|
           menu.choice "See All Teams", -> {self.user.list_teams}
           menu.choice "See All Heros"
           menu.choice "Create a Team"
           menu.choice "Edit a Team"
+          
         end
       end
 end
