0765fe6bafd8cfc514c56fa110259c1d38a29edd
Gulnoza Muminova <gmcharmy@gmail.com>
2019-07-30 09:47:58 -0400

    interface methods

--- lib/interface.rb
+++ lib/interface.rb
@@ -4,4 +4,22 @@ class Interface
     def initialize()
       @prompt = TTY::Prompt.new
     end
+    def welcome
+        puts "Welcome to the Hero Application! 💪"
+          answer = prompt.select("Are you a returning user or are you a new user?") do |menu|
+          menu.choice "New User", -> {User.handle_new_user}
+          menu.choice "Returning User", -> {User.handle_returning_user}
+        end
+    end
+
+    def main_menu
+        system "clear"
+        user.reload
+        prompt.select("Welcome #{self.user.name}! What would you like to do today?") do |menu|
+          menu.choice "See All Teams", -> {self.user.list_teams}
+          menu.choice "See All Heros"
+          menu.choice "Create a Team"
+          menu.choice "Edit a Team"
+        end
+      end
 end
